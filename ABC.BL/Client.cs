2fb189c2963a55b095dbb1c666406f3d547727a1
Patryk Koler <pkoler@arpideas.pl>
2020-05-11 15:47:49 +0200

    Dodaj pliki projektów.

---
+++ ABC.BL/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.BL
+{
+    public class Client
+    {
+        public static int Licznik { get; set; }
+
+
+        private string _surname;
+
+        public string Surname{ get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int clientId { get; private set; }
+        public string nameSurname 
+        { get 
+            {
+                string namesurname = Name;
+                if (!string.IsNullOrWhiteSpace(Surname))
+                {
+                    if (!string.IsNullOrWhiteSpace(namesurname))
+                    {
+                        namesurname += ", ";
+                    }
+                    namesurname += Surname;
+                }
+                return namesurname; 
+            }
+        }
+    }
+}
---
+++ ABC.BLTest/ClientTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class ClientTest
+    {
+        [TestMethod]
+        public void imienazwiskotest()
+        {
+            //Ararnge(zaaranżuj test)
+            Client client = new Client();
+            client.Name = "Patryk";
+            client.Surname = "Koler";
+            string check = "Patryk, Koler";
+
+            //ACT (działaj)
+            string actual = client.nameSurname;
+
+            //Assect(potwierdzenie testu)
+            Assert.AreEqual(check, actual);
+        }
+        [TestMethod]
+        public void imiepuste()
+        {
+            //Ararnge(zaaranżuj test)
+            Client client = new Client();
+            client.Surname = "Koler";
+            string check = "Koler";
+
+            //ACT (działaj)
+            string actual = client.nameSurname;
+
+            //Assect(potwierdzenie testu)
+            Assert.AreEqual(check, actual);
+        }
+        [TestMethod]
+        public void nazwiskopuste()
+        {
+            //Ararnge(zaaranżuj test)
+            Client client = new Client();
+            client.Name = "Patryk";            
+            string check = "Patryk";
+
+            //ACT (działaj)
+            string actual = client.nameSurname;
+
+            //Assect(potwierdzenie testu)
+            Assert.AreEqual(check, actual);
+        }
+        [TestMethod]
+        public void statictest()
+        {
+            Client client1 = new Client();
+            client1.Name = "Patryk";
+            Client.Licznik += 1;
+
+            Client client2 = new Client();
+            client2.Name = "Magda";
+            Client.Licznik += 1;
+
+            Client client3 = new Client();
+            client3.Name = "Marek";
+            Client.Licznik += 1;
+
+            Assert.AreEqual(3, Client.Licznik);
+        }
+    }
+}
